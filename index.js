0bd591a96c29126679ade47fb815d82f8c9b4114
sandysanthoshsk <sandysanthosh251000@gmail.com>
2024-04-16 10:33:21 +0530

    new changes

---
+++ index.js
@@ -0,0 +1,17 @@
+// document.getElementById('left').style.flexDirection='row';
+// function swapimg(){
+//     document.getElementById('left').style.flexDirection='row-reverse';
+//     console.log("hiiii");
+// }
+// let houserent = document.getElementById('houserent').value
+function pgrooms() {
+    // pgrooms.style.display = none
+   document.getElementById('pgrooms').style.display = 'block';
+   document.getElementById('houserent').style.display = 'none';
+   console.log("Working.....");
+}
+function houserent() {
+   document.getElementById('houserent').style.display = 'block';
+   document.getElementById('pgrooms').style.display = 'none';
+
+}
